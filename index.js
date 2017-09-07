4f6320fbfbf747bc8d93b022fca8c07211fa14a9
AmandeepKumarBatra <31728598+AmandeepKumarBatra@users.noreply.github.com>
2017-09-07 17:47:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
-        speech: "deep",
+   if(speech=='hi'){
+        return res.json({
+        speech: "hello",
         displayText: "batra",
         source: 'webhook-echo-sample'
     });
+   }
+    else{
+         return res.json({
+        speech: speech,
+        displayText: "batra",
+        source: 'webhook-echo-sample'
+    });
+    }
+    
 });
 
 restService.post('/slack-test', function(req, res) {
