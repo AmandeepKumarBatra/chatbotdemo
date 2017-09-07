f0a0de8b03489c0543fadaedf11ccfd519fd7565
AmandeepKumarBatra <31728598+AmandeepKumarBatra@users.noreply.github.com>
2017-09-07 19:00:59 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,1 +1,24 @@
 
+'use strict';
+
+
+let request = require('request');
+
+let apiKey = 'b1b15e88fa797225412429c1c50c122a1';
+let city = 'London,uk';
+let url = 'http://samples.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}'
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
+
+
+
+
+
+
+//http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1
